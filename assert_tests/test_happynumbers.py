1a80b50c2f61cc519d1db74a498e69562ef5e745
vsapucaia <vsfortunato@gmail.com>
2016-09-03 09:47:50 -0300

    using subtests

--- assert_tests/test_happynumbers.py
+++ assert_tests/test_happynumbers.py
@@ -5,8 +5,11 @@ from assert_tests.happynumbers import happy_number
 class HappynumbersTest(unittest.TestCase):
     def test_number_happy(self):
         for n in (1, 7, 10, 13, 19, 100):
-            self.assertEqual(happy_number(n), True)
+            with self.subTest(n=n):
+                self.assertEqual(happy_number(n), True, 'Number %d is not happy.' % n)
 
     def test_number_not_happy(self):
         for n in (4, 35, 87, 90):
-            self.assertEqual(happy_number(n), False)
+            with self.subTest(n=n):
+                self.assertEqual(happy_number(n), False, 'Number %d is happy.' % n)
+
